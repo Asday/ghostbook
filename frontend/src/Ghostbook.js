53f94c28028034117273b72c24f87d03ee503baa
Adam Barnes <adam@jp74.com>
2017-03-28 22:14:34 +0100

    Added some error handling, added a comment, and refactored comment fetching to its own function.

--- frontend/src/Ghostbook.js
+++ frontend/src/Ghostbook.js
@@ -13,24 +13,45 @@ class Ghostbook extends Component {
     this.state = {
       comments: [],
       commentsLoaded: false,
+      failedToLoadComments: false,
       comment: "",
     };
   }
 
-  componentDidMount() {
+  _fetchComments() {
     const {
       ghostbookId,
       ghostbookCommentsRoot,
     } = this.props;
 
+    // TODO:  This spews 404-related errors in the console when the
+    // comments thread doesn't exist yet.  Apparently that's unfixable,
+    // but it sucks and I hate it.
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=124534
+    // suggests this is a wontfix, which also sucks.
+    //
+    // Until then, I'm spewing a console log to assuage passing console
+    // peekers' fears.
     fetch(`${ghostbookCommentsRoot}${ghostbookId}.json`)
       .then((response) => {
         if (response.ok) {
           response.json().then((comments) => {
             this.setState({comments, commentsLoaded: true});
           });
+        } else {
+          this.setState({failedToLoadComments: true});
+        }
+      })
+      .catch((error) => {
+        if (error.name === 'TypeError' && error.message === 'Failed to fetch') {
+          console.log('The above error, if your browser displayed one, was expected; no ghostbook comments exist for this ID yet.');
+          this.setState({commentsLoaded: true});
         }
       });
+  }
+
+  componentDidMount() {
+    this._fetchComments();
   }
 
   render() {
