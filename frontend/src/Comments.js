4c3cf1e04bddb7f6d0385831c59a46acff007cd3
Adam Barnes <adam@jp74.com>
2017-03-28 23:01:57 +0100

    Rendered comments.

--- frontend/src/Comments.js
+++ frontend/src/Comments.js
@@ -1,13 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 
+import { map } from 'lodash';
+
 class Comments extends Component {
   static propTypes = {
     comments: PropTypes.arrayOf(PropTypes.shape({
-      timestamp: PropTypes.number.isRequired,
       comment: PropTypes.string.isRequired,
+      timestamp: PropTypes.number.isRequired,
     })).isRequired,
     commentsLoaded: PropTypes.bool.isRequired,
     failedToLoadComments: PropTypes.bool.isRequired,
+  }
+
+  _renderComment(commentData, key) {
+    const {
+      comment,
+      timestamp,
+    } = commentData;
+
+    const date = new Date(timestamp * 1000);
+    const humanReadableDate = date.toString();
+
+    return (
+      <section key={ key }>
+        <time dateTime={ timestamp }>{ humanReadableDate }</time>
+        <p>{ comment }</p>
+      </section>
+    );
   }
 
   render() {
@@ -16,7 +35,9 @@ class Comments extends Component {
     } = this.props;
 
     return (
-      <div>{ comments.length }</div>
+      <section>
+        { map(comments, this._renderComment) }
+      </section>
     );
   }
 }
